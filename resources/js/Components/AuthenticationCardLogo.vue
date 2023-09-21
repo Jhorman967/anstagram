20efb1486c66ff406f06268a58bae984f436218c
Jhorman967 <jhorman967@gmail.com>
2023-09-20 20:23:23 -0500

    new commit

---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link :href="'/'">
+        <img class="w-60" :src="image" alt="Logo anstagram">
+    </Link>
+</template>
+
+<script>
+    import image from '../../images/anstagram.jpeg'
+
+    export default {
+        data(){
+            return{
+                image:image
+            }
+        }
+    }
+</script>
+
+
